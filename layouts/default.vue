34f872e4af03583cd64601f51de2b38df4d0c329
OlaleyeFisayo <festusolaleyef@gmail.com>
2025-05-24 14:50:14 +0100

    minor changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
-  <section class="w-full min-h-[100dvh]">
+  <section class="flex flex-col min-h-[100dvh]">
     <AppNavBar />
-    <slot />
+    <main class="px-8 flex-1 flex flex-col">
+      <section class="container mx-auto h-full flex-1">
+        <slot />
+      </section>
+    </main>
   </section>
 </template>
