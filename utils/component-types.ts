8a6f9dc97ead0463eeb46921d969dd1ef4e0275f
OlaleyeFisayo <festusolaleyef@gmail.com>
2025-05-25 23:58:39 +0100

    refactor: fewer and modularized code

---
+++ utils/component-types.ts
@@ -0,0 +1,20 @@
+export type ComponentSize = "xs" | "sm" | "md" | "lg" | "xl" | undefined;
+
+export type ComponentVariant =
+  | "solid"
+  | "outline"
+  | "soft"
+  | "subtle"
+  | "ghost"
+  | "link"
+  | undefined;
+
+export type ComponentColor =
+  | "neutral"
+  | "error"
+  | "warning"
+  | "warning"
+  | "success"
+  | "primary"
+  | "secondary"
+  | undefined;
