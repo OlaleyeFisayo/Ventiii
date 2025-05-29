dd78342c5504c04003880dd85bae053667a77c67
OlaleyeFisayo <festusolaleyef@gmail.com>
2025-05-29 22:40:31 +0100

    setup: nodemailer setup

---
+++ lib/email/index.ts
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+
+import env from "../env";
+
+const transporter = nodemailer.createTransport({
+  host: env.SMTP_HOST,
+  port: env.SMTP_PORT,
+  secure: true,
+  auth: {
+    user: env.SMTP_USER,
+    pass: env.SMTP_PASS,
+  },
+});
+
+export async function sendEmail(opts: {
+  to: string;
+  subject: string;
+  text: string;
+}) {
+  await transporter.sendMail({
+    from: env.SMTP_USER,
+    to: opts.to,
+    subject: opts.subject,
+    text: opts.text,
+  });
+}
