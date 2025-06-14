41c905b703f6ea335b259e49d2432e0762f3022f
OlaleyeFisayo <festusolaleyef@gmail.com>
2025-06-14 13:09:13 +0100

    setup: event layout

---
+++ layouts/event.vue
@@ -0,0 +1,6 @@
+<template>
+  <section class="text-black">
+    <h1>This is the layout for events</h1>
+    <slot />
+  </section>
+</template>
--- pages/event/[eventId]/index.vue
+++ pages/event/[eventId]/index.vue
@@ -1,3 +1,9 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: "event",
+});
+</script>
+
 <template>
   <h1>This Event</h1>
 </template>
