eeb203a0261c5ce6e45a20e63ccedfed1407d931
OlaleyeFisayo <festusolaleyef@gmail.com>
2025-07-10 15:14:35 +0100

    chore: made event landing-page

--- pages/event/[eventId]/index.vue
+++ pages/event/[eventId]/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const route = useRoute();
+const eventId = route.params.eventId;
+const defaultEventUrl = `/event/${eventId}`;
+</script>
+
 <template>
   <section>
     <section class="flex gap-2 justify-between items-center">
@@ -13,6 +19,7 @@
         label="View Landing Page"
         icon="i-tabler-eye"
         class="border-2! bg-inherit! text-black! rounded-2xl! hover:bg-gray-100!"
+        :to="`${defaultEventUrl}/landing-page`"
       />
     </section>
   </section>
---
+++ pages/event/[eventId]/landing-page/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+});
+</script>
+
+<template>
+  <section>
+    <h1>Event Landing Page</h1>
+  </section>
+</template>
